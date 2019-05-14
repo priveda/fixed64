f1fb565e39a1583f48b17a1aecfc7e4e99c82159
balarabe@protonmail.com <balarabe@protonmail.com>
2019-05-14 18:59:47 +0100

    Fix package name in .GoString()

--- go_string.go
+++ go_string.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) admin@priveda.com                                            License: MIT
-// :v: 2019-05-14 17:45:40 5D8A3A                 priveda/fixed64/[go_string.go]
+// :v: 2019-05-14 18:59:36 7D10F5                 priveda/fixed64/[go_string.go]
 // -----------------------------------------------------------------------------
 
 package fixed64
@@ -8,7 +8,7 @@ package fixed64
 // GoString outputs the value as a Go language string,
 // It implements the fmt.GoStringer interface.
 func (n Fixed64) GoString() string {
-	return "zr.Fixed64Of(" + n.String() + ")"
+	return "fixed64.Fixed64Of(" + n.String() + ")"
 }
 
 //end
