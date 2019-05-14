7260705e0973fb93d0ab033848c377da374591ee
balarabe@protonmail.com <balarabe@protonmail.com>
2019-05-14 19:01:29 +0100

    Let UnmarshalJSON() call mod.Error() (to log)

--- unmarshal_json.go
+++ unmarshal_json.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) admin@priveda.com                                            License: MIT
-// :v: 2019-05-14 17:45:40 BAFEF0            priveda/fixed64/[unmarshal_json.go]
+// :v: 2019-05-14 19:01:20 6A9D2A            priveda/fixed64/[unmarshal_json.go]
 // -----------------------------------------------------------------------------
 
 package fixed64
@@ -12,7 +12,7 @@ import (
 // UnmarshalJSON unmarshals a JSON description of Fixed64.
 // This method alters the number's value.
 func (n *Fixed64) UnmarshalJSON(data []byte) error {
-	//   ^  don't remove pointer receiver, it is necessary
+	//  ^  don't remove pointer receiver, it is necessary
 	if n == nil {
 		return mod.Error(ENilReceiver)
 	}
@@ -21,7 +21,7 @@ func (n *Fixed64) UnmarshalJSON(data []byte) error {
 		err = json.Unmarshal(data, &num)
 	)
 	if err != nil {
-		return err
+		return mod.Error(err)
 	}
 	n.i64 = int64(num * 1E4)
 	return nil
