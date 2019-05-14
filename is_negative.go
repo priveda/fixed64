f2f207ab8b3b875acf41ed7225e2f6a7716dabdc
balarabe@protonmail.com <balarabe@protonmail.com>
2019-05-14 19:00:38 +0100

    Let IsNegative() return false if n == NaN

--- is_negative.go
+++ is_negative.go
@@ -1,13 +1,13 @@
 // -----------------------------------------------------------------------------
 // (c) admin@priveda.com                                            License: MIT
-// :v: 2019-05-14 17:45:40 F20EB4               priveda/fixed64/[is_negative.go]
+// :v: 2019-05-14 19:00:28 482AE0               priveda/fixed64/[is_negative.go]
 // -----------------------------------------------------------------------------
 
 package fixed64
 
 // IsNegative returns true if the number is negative.
 func (n Fixed64) IsNegative() bool {
-	return n.i64 < 0
+	return n.i64 < 0 && n.i64 != NaN
 }
 
 //end
