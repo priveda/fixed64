9a71254c0b18a85df58e61cd83ea50e8437c5c5e
balarabe@protonmail.com <balarabe@protonmail.com>
2019-05-14 18:57:40 +0100

    Let String() return a blank if =NaN, "0" if =0

--- string.go
+++ string.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) admin@priveda.com                                            License: MIT
-// :v: 2019-05-14 18:46:08 EAD6F2                    priveda/fixed64/[string.go]
+// :v: 2019-05-14 18:57:21 FECEBB                    priveda/fixed64/[string.go]
 // -----------------------------------------------------------------------------
 
 package fixed64
@@ -13,6 +13,16 @@ import (
 // String returns a string representing the fixed-point number
 // with up to 4 decimals and implements the Stringer interface.
 func (n Fixed64) String() string {
+	switch n.i64 {
+	case NaN:
+		{
+			return ""
+		}
+	case 0:
+		{
+			return "0"
+		}
+	}
 	var (
 		i    = n.i64 / 1E4              // integer value
 		d    = n.i64 - i*1E4            // decimal value
