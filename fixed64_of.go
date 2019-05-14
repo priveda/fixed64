567349a263eab6837f8fc99313932afa4eeede51
balarabe@protonmail.com <balarabe@protonmail.com>
2019-05-14 16:29:20 +0100

    Format case blocks

--- fixed64_of.go
+++ fixed64_of.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) admin@priveda.com                                            License: MIT
-// :v: 2019-05-14 16:27:00 BB4802                priveda/fixed64/[fixed64_of.go]
+// :v: 2019-05-14 16:29:06 FE03B7                priveda/fixed64/[fixed64_of.go]
 // -----------------------------------------------------------------------------
 
 package fixed64
@@ -15,55 +15,76 @@ import (
 // not numeric, logs an error and sets the Fixed64 to zero.
 func Fixed64Of(value interface{}) Fixed64 {
 	switch val := value.(type) {
-	//
-	// Fixed64 already?
 	case Fixed64:
-		return val
-	//
+		{
+			return val
+		}
+
 	// integers
 	case int8:
-		return Fixed64{int64(val) * 1E4}
-	case int16:
-		return Fixed64{int64(val) * 1E4}
-	case int32: //                              int32 and int could exceed range
-		return Fixed64Of(int64(val))
-	case int:
-		return Fixed64Of(int64(val))
-	case int64:
-		if val < -IntLimit || val > IntLimit {
-			return currencyOverflow(val < 0, EOverflow, ": ", val)
+		{
+			return Fixed64{int64(val) * 1E4}
 		}
-		return Fixed64{int64(val) * 1E4}
-	//
+	case int16:
+		{
+			return Fixed64{int64(val) * 1E4}
+		}
+	case int32: //                              int32 and int could exceed range
+		{
+			return Fixed64Of(int64(val))
+		}
+	case int:
+		{
+			return Fixed64Of(int64(val))
+		}
+	case int64:
+		{
+			if val < -IntLimit || val > IntLimit {
+				return currencyOverflow(val < 0, EOverflow, ": ", val)
+			}
+			return Fixed64{int64(val) * 1E4}
+		}
 	// unsigned integers
 	case uint8:
-		return Fixed64{int64(val) * 1E4}
-	case uint16:
-		return Fixed64{int64(val) * 1E4}
-	case uint32: //                           uint32 and uint could exceed range
-		return Fixed64Of(uint64(val))
-	case uint:
-		return Fixed64Of(uint64(val))
-	case uint64:
-		if val > IntLimit {
-			return currencyOverflow(false, EOverflow, "uint64: ", val)
+		{
+			return Fixed64{int64(val) * 1E4}
 		}
-		return Fixed64{int64(val) * 1E4}
-	//
+	case uint16:
+		{
+			return Fixed64{int64(val) * 1E4}
+		}
+	case uint32: //                           uint32 and uint could exceed range
+		{
+			return Fixed64Of(uint64(val))
+		}
+	case uint:
+		{
+			return Fixed64Of(uint64(val))
+		}
+	case uint64:
+		{
+			if val > IntLimit {
+				return currencyOverflow(false, EOverflow, "uint64: ", val)
+			}
+			return Fixed64{int64(val) * 1E4}
+		}
 	// float
 	case float32:
-		if val < -float32(IntLimit)-0.9999 ||
-			val > float32(IntLimit)+0.9999 {
-			return currencyOverflow(val < 0, EOverflow, "float32: ", val)
+		{
+			if val < -float32(IntLimit)-0.9999 ||
+				val > float32(IntLimit)+0.9999 {
+				return currencyOverflow(val < 0, EOverflow, "float32: ", val)
+			}
+			return Fixed64{int64(float64(val) * 1E4)}
 		}
-		return Fixed64{int64(float64(val) * 1E4)}
 	case float64:
-		if val < -float64(IntLimit)-0.9999 ||
-			val > float64(IntLimit)+0.9999 {
-			return currencyOverflow(val < 0, EOverflow, "float64: ", val)
+		{
+			if val < -float64(IntLimit)-0.9999 ||
+				val > float64(IntLimit)+0.9999 {
+				return currencyOverflow(val < 0, EOverflow, "float64: ", val)
+			}
+			return Fixed64{int64(val * 1E4)}
 		}
-		return Fixed64{int64(val * 1E4)}
-	//
 	// integer pointers
 	case *int:
 		if val != nil {
@@ -117,13 +138,17 @@ func Fixed64Of(value interface{}) Fixed64 {
 		}
 	// strings
 	case string:
-		return Fixed64OfS(val)
+		{
+			return Fixed64OfS(val)
+		}
 	case *string:
 		if val != nil {
 			return Fixed64OfS(*val)
 		}
 	case fmt.Stringer:
-		return Fixed64OfS(val.String())
+		{
+			return Fixed64OfS(val.String())
+		}
 	}
 	mod.Error("Type", reflect.TypeOf(value), "not handled; =", value)
 	return Fixed64{}
